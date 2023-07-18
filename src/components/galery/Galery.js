aeb348c76b3fb6a01bdd2854aa6aceba90c75565
Lelik Jan <21alex7777@gmail.com>
2023-07-18 15:31:20 +0300

    feat: add Galery.js

--- src/components/galery/Galery.js
+++ src/components/galery/Galery.js
@@ -1,17 +1,39 @@
-import { sculptureList } from './data.js';
+import { sculptureList } from "./data.js";
+import { useState } from "react";
 
 export default function Gallery() {
-  let index = 0;
+  //let index = 0;
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+
+  let hasPrev = index > 0;
+  let hasNext = index < sculptureList.length - 1;
 
   function handleClick() {
-    index = index + 1;
-  }
+    // index = index + 1;
+    if (hasNext) //setIsDone(true);
+    setIndex(index + 1);
 
+  }
+  function handleClickBack() {
+    // index = index + 1;
+    if (hasPrev) //setIsDone(false);
+    setIndex(index - 1);
+
+  }
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
   let sculpture = sculptureList[index];
+
   return (
     <>
-      <button onClick={handleClick}>
+      <button disabled={!hasNext} onClick={handleClick}>
         Next
+      </button>
+      <button disabled={!hasPrev} onClick={handleClickBack}>
+        Back
       </button>
       <h2>
         <i>{sculpture.name} </i>
@@ -20,13 +42,12 @@ export default function Gallery() {
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
-      <img
-        src={sculpture.url}
-        alt={sculpture.alt}
-      />
-      <p>
-        {sculpture.description}
-      </p>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>{sculpture.description}</p>
     </>
   );
 }
