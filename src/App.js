a40aa81e021b5be486cafd528b5a584a29e6bea2
Lelik Jan <21alex7777@gmail.com>
2023-07-18 16:25:45 +0300

    feat:add Form.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Form from "./components/form/Form";
 import Gallery from "./components/galery/Galery";
 
 function App() {
   return (
     <div className="App">
       <Gallery />
+      <Form />
     </div>
   );
 }
